910750f47a49e8bf9e3c8a7724b38df30e87ba3d
Tao Zanella <tao.zanella@gmail.com>
2024-01-22 16:05:42 -0300

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello World');
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    res.json( { message: 'test123456' } );
+});
+
+const port = Number(process.env.PORT) || 2000;
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
